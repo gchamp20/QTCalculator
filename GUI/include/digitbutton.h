8c633912bb070563f9fd6b48f5a955bca9974d35
Guillaume <champagne.guillaume.c@gmail.com>
2015-01-22 22:58:56 -0500

    Added comments

--- GUI/include/digitbutton.h
+++ GUI/include/digitbutton.h
@@ -1,21 +1,57 @@
 #ifndef DIGITBUTTON_H
 #define DIGITBUTTON_H
-
 #include <QPushButton>
+
+/*!
+ *\file digitbutton.h
+ * \Author Guillaume Champagne
+ * \date 22 January 2015
+ * \brief The DigitButton class
+ */
+
+//!DigitButton class
+/*!
+ * Object that represent the calculator numeric buttons
+ */
 
 class DigitButton : public QPushButton
 {
     Q_OBJECT
 public:
+
+    /*!
+     * \brief DigitButton Constructor
+     * \param parent Parent QWidget (Default set to null)
+     */
     explicit DigitButton(QWidget *parent = 0);
+
+    /*!
+     * \brief DigitButton Constructor
+     * \param text String written on the button
+     * \param parent Parent QWidget (Default set to null
+     */
+    explicit DigitButton(const QString& text, QWidget *parent = 0);
+
+    /*!
+      *\brief DigitButton destructor
+      */
     ~DigitButton();
 
 signals:
 
+    /*!
+     * \brief pressed Signas trigged on button pressed event
+     * \param value Integer value of the button
+     */
+    void pressed(int value);
 public slots:
 
 private:
 
+    /*!
+     * \brief value_ Integer value of the button
+     */
+    int value_;
 };
 
 #endif // DIGITBUTTON_H
